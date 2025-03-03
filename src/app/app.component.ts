e15b5e84c6875bf9252b792fe561f6e27b5527a3
jjuu28 <erdemsueral@gmail.com>
2025-03-03 17:36:26 +0100

    Login noch angepasst und alternativen für Footer Anzeige eingebaut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {FooterComponent} from './core/footer/footer.component';
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,5 +19,17 @@ export class AppComponent {
     this.router.events.subscribe(() => {
       // Wenn die aktuelle URL '/login' ist, blende den Footer aus
       this.showFooter = this.router.url !== '/login';    });
+
+    /* So muss es aussehen wenn footer auf anderen Seiten ausgeblendet werden soll
+
+        this.router.events.subscribe(() => {
+      // Liste von Seiten, auf denen der Footer NICHT angezeigt werden soll
+      const hiddenFooterRoutes = ['/login', '/register', '/privacy', '/impressum'];
+
+      // Wenn die aktuelle Route in der Liste ist, verstecke den Footer
+      this.showFooter = !hiddenFooterRoutes.includes(this.router.url);
+     */
+
   }
+
 }
