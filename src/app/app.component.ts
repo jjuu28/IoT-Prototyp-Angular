0ae0b8511dedf8a9224c94505ae14cf59ebc2e2f
jjuu28 <erdemsueral@gmail.com>
2025-03-03 17:33:51 +0100

    Footer eingebaut und überprüfung eingebaut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import {FooterComponent} from './core/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularTest';
+
+  showFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      // Wenn die aktuelle URL '/login' ist, blende den Footer aus
+      this.showFooter = this.router.url !== '/login';    });
+  }
 }
