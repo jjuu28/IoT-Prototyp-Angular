45ece4e9fbfe963984a97938a3e467e861e0e8ce
jjuu28 <erdemsueral@gmail.com>
2025-03-10 00:47:07 +0100

    Home struktur, und Komponent (ohne Service anpassung (SensorService))

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Router} from '@angular/router';
 
@@ -8,6 +8,53 @@ import {Router} from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  username: string = '';
+  messages: any = null;
+  sensorChanges: any[] = [];
+  authToken: string | null = localStorage.getItem('authToken') || sessionStorage.getItem('authToken');
+
+
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    if (!this.authToken) {
+      console.warn('No auth token found, redirecting to login');
+      this.router.navigate(['/login']);
+    } else {
+      this.loadUsername();
+      this.loadwarnings();
+      this.loadsensordata();
+    }
+
+  }
+
+  loadUsername() {
+    this.username = 'test';
+  }
+
+  loadwarnings() {
+    this.sensorService.getWarnings().subscribe(
+      (messages) => {
+        this.messages = messages;
+      },
+      (error) => console.error("Fehler beim Abrufen der wichtigen Meldungen", error)
+    );
+  }
+
+  loadsensordata() {
+    this.sensorService.getSensorChanges().subscribe(
+      (sensorChanges) => {
+        this.sensorChanges = sensorChanges;
+      },
+      (error) => console.error("Fehler beim Abrufen der Sensordaten", error)
+    );
+  }
+
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
 
 }
